81796e72c71464291dd28b2b52330c6886da6442
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-07-24 17:15:19 +0530

    card added

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Summary from './Summary'
+import React from "react";
+import Summary from "./Summary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
   return (
     <>
-      <Summary/>
+      <Summary />
+      <main>
+        <AvailableMeals />
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Meals
+export default Meals;
---
+++ src/Components/Meals/MealsItems/MealsForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealsForm = () => {
+  return <div></div>;
+};
+
+export default MealsForm;
